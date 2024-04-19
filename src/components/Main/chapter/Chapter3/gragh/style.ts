8bd88dae274e18e074399426386319cbb642ee4e
bae0203 <gmbae06@dgsw.hs.kr>
2024-04-19 09:33:08 +0900

    fix : build Error

--- src/components/Main/chapter/Chapter3/gragh/style.ts
+++ src/components/Main/chapter/Chapter3/gragh/style.ts
@@ -1,35 +1,35 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-export const MainGraghLayout = styled.div`
-  ${({ width, height }: { width: number; height: number }) =>
+export const MainGraghLayout = styled.div<{ width: number; height: number }>`
+  ${({ width, height }) =>
     `width: ${width}px;
     height: ${height}px;`};
   display: flex;
   justify-content: space-between;
   align-items: flex-end;
-`
+`;
 
-export const GraghContentWrapCol = styled.div`
+export const GraghContentWrapCol = styled.div<{ height: number }>`
   width: 46px;
-  height: ${({ height }: { height: number }) => height}px;
-`
+  height: ${({ height }) => height}px;
+`;
 
 interface IGragh {
-  height: number
-  startColor: string
-  endColor: string
+  height: number;
+  startColor: string;
+  endColor: string;
 }
 
-export const Gragh = styled.div`
+export const Gragh = styled.div<IGragh>`
   background: linear-gradient(
-    ${({ startColor, endColor }: IGragh) => `${startColor} ,${endColor}`}
+    ${({ startColor, endColor }) => `${startColor} ,${endColor}`}
   );
-  height: ${({ height }: IGragh) => height}px;
+  height: ${({ height }) => height}px;
   border-radius: 24px 24px 0px 0px;
-`
+`;
 
 export const GraghParagragh = styled.p`
   color: #424344;
   font-size: 1rem;
   margin-bottom: 1rem;
-`
+`;
